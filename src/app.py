1ca82d335a8061be73b2198f37947a7a4d1f16fa
hzoghi <hamed.zoghi@gmail.com>
2020-12-13 11:28:56 -0500

    add index.html and update app to render it

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
 """
     Simple flask app to create a server for the todo app and handle the database
 """
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello'
+    return render_template('index.html')
