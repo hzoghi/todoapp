96f93ce908775a7addac7e50d3c91b78a4221aeb
hzoghi <hamed.zoghi@gmail.com>
2020-12-13 12:20:44 -0500

    update app to read database

--- src/app.py
+++ src/app.py
@@ -2,15 +2,24 @@
     Simple flask app to create a server for the todo app and handle the database
 """
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-app=Flask(__name__)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hamed.zoghi@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db=SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__= 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String, nullable = False)
+
+    def __repr__():
+        return f'<Todo id: {self.id}, decription: {self.description}>'
+
+db.create_all()
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        'description': 'TODO 1'
-    },{
-        'description': 'TODO 2'
-    }, {
-        'description': 'TODO 3'
-    }])
+    return render_template('index.html', data=Todo.query.all())
