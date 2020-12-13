13b201c90147e38e731939dc4d25103de4445b3a
hzoghi <hamed.zoghi@gmail.com>
2020-12-13 11:35:27 -0500

    updated index & app to pass the data from app

--- src/app.py
+++ src/app.py
@@ -7,4 +7,10 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data=[{
+        'description': 'TODO 1'
+    },{
+        'description': 'TODO 2'
+    }, {
+        'description': 'TODO 3'
+    }])
