691e4ad27d8cf47cbfc6da9f04e7803616a4da0c
hzoghi <hamed.zoghi@gmail.com>
2020-12-21 23:56:42 -0500

    migration files and modified app

--- src/app.py
+++ src/app.py
@@ -4,21 +4,25 @@
 import sys
 from flask import Flask, render_template, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hamed.zoghi@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 class Todo(db.Model):
     __tablename__= 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String, nullable = False)
+    completed = db.Column(db.Boolean, nullable=False, default = False)
 
     def __repr__():
         return f'<Todo id: {self.id}, decription: {self.description}>'
 
-db.create_all()
+#db.create_all()
 
 
 @app.route('/')
