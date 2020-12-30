14148f539d49e20a91d8519c1a8b1ca8ee9569f3
hzoghi <hamed.zoghi@gmail.com>
2020-12-30 16:06:37 -0500

    implemented the server side

--- src/app.py
+++ src/app.py
@@ -64,4 +64,14 @@ def set_completed_todo(todo_id):
         db.session.close()
     return redirect(url_for('index'))
 
+@app.route('/todos/<todo_id>/delete', methods=['GET'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
 
