6e9d01e3d12ed9e10f9d6a5eb9364c705d316c33
Hamed Zoghi <hamed.zoghi@gmail.com>
2020-12-31 21:34:19 -0500

    Feature/enable lists data model (#6)
    * added TodoList class and updated Todo class
    
    * fixed syntax issue in __repr__ method in Todo
    
    * fixed minor spelling issue
    
    * added migration files and upgraded db

--- src/app.py
+++ src/app.py
@@ -18,11 +18,19 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String, nullable = False)
     completed = db.Column(db.Boolean, nullable=False, default = False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__():
-        return f'<Todo id: {self.id}, decription: {self.description}>'
+        return f'<Todo id: {self.id}, decription: {self.description}, completed: {self.completed}, list_id = {self.list_id}>'
 
-#db.create_all()
+class TodoList(db.Model):
+    __tablename__='todolists'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(), nullable = False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
+
+    def __repr__():
+        return f'<TodoList id: {self.id}, name: {self.name}>'
 
 
 @app.route('/')
