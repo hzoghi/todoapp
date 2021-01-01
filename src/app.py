8b35febca26336014792b2fb6a060bf2e4fa78de
hzoghi <hamed.zoghi@gmail.com>
2020-12-31 21:27:55 -0500

    added migration files and upgraded db

--- src/app.py
+++ src/app.py
@@ -18,7 +18,7 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String, nullable = False)
     completed = db.Column(db.Boolean, nullable=False, default = False)
-    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False )
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__():
         return f'<Todo id: {self.id}, decription: {self.description}, completed: {self.completed}, list_id = {self.list_id}>'
